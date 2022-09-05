ea9cf0681cfa741c5cf6a3d3276aa42346774927
debby <app2984@gmail.com>
2022-09-05 10:24:00 +0800

    add activity content route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Profile from './pages/Profile/Profile'
 import Activity from './pages/Activity/Activity'
+import ActivityContent from './pages/Activity/ActivityContent'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
@@ -15,6 +16,7 @@ root.render(
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
         <Route path="activity" element={<Activity />} />
+        <Route path="activity/:id" element={<ActivityContent />} />
       </Route>
     </Routes>
   </BrowserRouter>,
