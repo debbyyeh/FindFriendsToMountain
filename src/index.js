1d5355e523fa2501d0add0c2d69af40abd144a56
debby <app2984@gmail.com>
2022-09-05 10:22:41 +0800

    add route to activity

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Profile from './pages/Profile/Profile'
+import Activity from './pages/Activity/Activity'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
@@ -13,6 +14,7 @@ root.render(
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="activity" element={<Activity />} />
       </Route>
     </Routes>
   </BrowserRouter>,
